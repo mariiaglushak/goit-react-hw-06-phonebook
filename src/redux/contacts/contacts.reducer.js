2318b941512809d9acf4059acaff402798cebdfd
mariiaglushak <90639229+mariiaglushak@users.noreply.github.com>
2023-11-23 09:52:36 +0200

    поправка

--- src/redux/contacts/contacts.reducer.js
+++ src/redux/contacts/contacts.reducer.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState={
-    contacts: JSON.parse(localStorage.getItem('contacts')) ?? []
+    contacts: []
 }
 
 const contactsSlice = createSlice({
